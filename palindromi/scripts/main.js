7601fdec8801cc3f3c52e9fbf040fff03614f5ad
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-07-10 18:30:55 +0200

    comments added, html detail added

--- palindromi/scripts/main.js
+++ palindromi/scripts/main.js
@@ -1,21 +1,34 @@
-Variables for html
+// Variables for html elements.
 
 const inputWordElement = document.getElementById("input_word");
 const checkElement = document.getElementById("check");
 const resultElement = document.getElementById("result");
 
+// Event listener to start program.
+
 checkElement.addEventListener("click", function() {
+
+    // Gets word to check
 
 const wordToCheck = inputWordElement.value;
 
+/* Calls function to check if the word is a palindrome, 
+saves its return in the "result" variable. */
+
 const result = wordChecker(wordToCheck);
+
+// Output
 
 resultElement.innerHTML = "Inserted word is " + result;
 
 }
 )
 
-
+/**
+ * Checks if a word is a palindrome
+ * @param {string} wordToCheckParameter
+ * @returns {string}
+ */
 function wordChecker (wordToCheckParameter) {
 
     const k = wordToCheckParameter.length;
@@ -24,11 +37,11 @@ function wordChecker (wordToCheckParameter) {
 
         if (wordToCheckParameter.charAt(i) !== wordToCheckParameter.charAt(k - 1 - i)) {
 
-            return "is not palindrome";
+            return "is not a palindrome";
 
         } else {
             
-            return "is palindrome";
+            return "is a palindrome";
         
         }
 
