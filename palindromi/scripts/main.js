7be2a00e8016b78d46b6acf35b01f79e260a1900
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-07-10 18:24:45 +0200

    algorithm done

--- palindromi/scripts/main.js
+++ palindromi/scripts/main.js
@@ -1,2 +1,44 @@
+Variables for html
+
 const inputWordElement = document.getElementById("input_word");
-const checkelement = document.getElementById("check");
+const checkElement = document.getElementById("check");
+const resultElement = document.getElementById("result");
+
+checkElement.addEventListener("click", function() {
+
+const wordToCheck = inputWordElement.value;
+
+const result = wordChecker(wordToCheck);
+
+resultElement.innerHTML = "Inserted word is " + result;
+
+}
+)
+
+
+function wordChecker (wordToCheckParameter) {
+
+    const k = wordToCheckParameter.length;
+
+    for (let i = 0; i < (k / 2); i++) {
+
+        if (wordToCheckParameter.charAt(i) !== wordToCheckParameter.charAt(k - 1 - i)) {
+
+            return "is not palindrome";
+
+        } else {
+            
+            return "is palindrome";
+        
+        }
+
+    }
+
+}
+
+
+
+
+
+
+    
